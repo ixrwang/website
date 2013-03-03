a3570581f0066590e8822ce79a93db1acbbfdbb5
IXR <IXR@IXR-PC>
2013-03-03 19:40:42 +0800

    初始化项目

---
+++ website-app/src/main/java/name/ixr/website/app/App.java
@@ -0,0 +1,13 @@
+package name.ixr.website.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
