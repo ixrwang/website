2916fc1328d57d09f8a43dbb04ff34df7d91e8d6
IXR <IXR@IXR-PC>
2013-03-06 02:18:55 +0800

    测试输出到logger

--- website-web/src/test/java/name/ixr/website/web/QQSdkTest.java
+++ website-web/src/test/java/name/ixr/website/web/QQSdkTest.java
@@ -5,20 +5,27 @@
 package name.ixr.website.web;
 
 import com.qq.connect.oauth.Oauth;
-import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * 对QQ的测试
  * @author IXR
  */
-public class QQSdkTest extends TestCase{
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/META-INF/root-context.xml","classpath:/META-INF/servlet-context.xml"})
+public class QQSdkTest extends AbstractJUnit4SpringContextTests{
     
-   public void testOauth() throws Exception {
+    @Test
+   public void oauth() throws Exception {
        MockHttpServletRequest request = new MockHttpServletRequest();
        request.setSession(new MockHttpSession());
        String url = new Oauth().getAuthorizeURL(request);
-       System.out.println(url);
+       logger.debug(url);
    } 
 }
