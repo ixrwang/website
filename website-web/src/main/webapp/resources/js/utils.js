2b041284928618efd9ffc090fe75a863c8048c40
IXR <IXR@IXR-PC>
2013-03-06 22:58:15 +0800

    添加内容

--- website-web/src/main/webapp/resources/js/utils.js
+++ website-web/src/main/webapp/resources/js/utils.js
@@ -1,14 +1,6 @@
-function setTheme(name) {
-setCookie('clienttheme',name);
-window.location.reload();
-}
-function setLanguage(name) {
-setCookie('clientlanguage',name);
-window.location.reload();
-}
-function setCookie(name,value){   
-var Days = 30;
-var exp  = new Date();
-exp.setTime(exp.getTime() + Days*24*60*60*1000);
-document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
 }
