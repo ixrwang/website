349867dd6dd61d9015db762d6c1794e7ff14c590
IXR <IXR@IXR-PC>
2013-03-09 17:54:54 +0800

    修复MVN配置

--- website-web/src/test/java/name/ixr/website/web/PostWeiXinTest.java
+++ website-web/src/test/java/name/ixr/website/web/PostWeiXinTest.java
@@ -4,12 +4,7 @@
  */
 package name.ixr.website.web;
 
-import com.qq.connect.utils.http.HttpClient;
-import com.qq.connect.utils.http.PostParameter;
-import com.qq.connect.utils.http.Response;
-import java.util.Map;
 import junit.framework.TestSuite;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 
 /**
@@ -20,18 +15,6 @@ public class PostWeiXinTest extends TestSuite{
     
     @Test
     public void test() throws Exception{
-        String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx7eb909445a938df1&secret=16625515439867caff6ca11660ea74b5";
-        HttpClient client = new HttpClient();
-        Response response = client.get(url);
-        String json = response.getResponseAsString();
-        ObjectMapper mapper  = new ObjectMapper();
-        Map<String,String> jsonObject = mapper.readValue(json, Map.class);
-        String access_token = jsonObject.get("access_token");
-        client = new HttpClient();
-        url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token="+access_token+"&next_openid=";
-        System.out.println(url);
-        response = client.post(url,new PostParameter[0]);
-        json = response.getResponseAsString();
-        System.out.println(json);
+        
     }
 }
