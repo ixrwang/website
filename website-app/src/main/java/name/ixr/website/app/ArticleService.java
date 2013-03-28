019013613cf4299f1f1b848d064536168323bd29
ixr_wang <mail@ixr.name>
2013-03-28 18:30:05 +0800

    修改样式

--- website-app/src/main/java/name/ixr/website/app/ArticleService.java
+++ website-app/src/main/java/name/ixr/website/app/ArticleService.java
@@ -4,24 +4,41 @@
  */
 package name.ixr.website.app;
 
+import java.util.List;
 import name.ixr.website.app.model.Article;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 /**
  * 文章信息
+ *
  * @author IXR
  */
 @Service
 public class ArticleService {
-    
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
+
+    public List<Article> queryByPage(int page, int size, String search) {
+        String sql = "SELECT * FROM t_article WHERE context like ? limit ?,?";
+        int start = (page - 1) * size;
+        int end = start + size;
+        Object[] param = new Object[]{"%" + search + "%", start, end};
+        return jdbcTemplate.query(sql, param, new BeanPropertyRowMapper<>(Article.class));
+    }
+
+    public int queryCount(String search) {
+        String sql = "SELECT COUNT(*) FROM t_article WHERE context like ?";
+        Object[] param = new Object[]{"%" + search + "%"};
+        return jdbcTemplate.queryForInt(sql, param);
+    }
+
     public int save(Article article) {
         String sql = "INSERT INTO t_article(id,uid,title,context,created) VALUES (?,?,?,?,?)";
-        Object[] param = new Object[]{article.getId(),article.getUid(),article.getTitle(),article.getContext(),article.getCreated()};
+        Object[] param = new Object[]{article.getId(), article.getUid(), article.getTitle(), article.getContext(), article.getCreated()};
         return jdbcTemplate.update(sql, param);
     }
 }
